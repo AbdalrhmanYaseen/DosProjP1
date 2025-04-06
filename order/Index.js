673f850a23b03195d6ce480034e71a62d9474f3c
mo3tasemjarar3a <s12112082@stu.najah.edu>
2025-04-06 17:20:45 +0200

    order service and db

--- order/Index.js
+++ order/Index.js
@@ -4,3 +4,31 @@ const axios = require('axios');
 const cors = require('cors');
 const app = express();
 
+const port = 4005;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/purch', async (req, res) => {
+    const { id, orderCost } = req.body;
+    const order = { id, orderCost };
+
+    try {
+        const response = await axios.post('http://catalog-server:3005/order', order);
+        console.log(response.data);
+        res.send({ message: 'Request sent to Catalog' });
+    } catch (err) {
+        console.error(err);
+        res.status(400).send({ error: err.message });
+    }
+});
+
+app.get('/test', (req, res) => {
+    res.send({ message: 'Arrive' });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
